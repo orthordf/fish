297c7f231fe81ed0ca8be4b6bb97e628dea78a50
Hirokazu Chiba <hchiba1@gmail.com>
2023-04-23 11:42:50 +0900

    Add fish/graph/ and index.html for blitzboard

---
+++ fish/graph/config.js
@@ -0,0 +1,20 @@
+
+{
+  node: {
+    caption: ['common', 'count'],
+    defaultIcon: true,
+    size: '@size',
+  },
+  edge: {
+    caption: ['count'],
+    width: '@width',
+    opacity: 0.6,
+  },
+  layout: 'default',
+  style: "background: white;",
+  extraOptions: {
+    interaction: {
+      navigationButtons: true
+    }
+  }
+}
